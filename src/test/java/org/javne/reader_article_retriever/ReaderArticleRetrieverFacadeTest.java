f010b0564b53951ddfa6225b3a5e2346abe618da
ewelinaborkowska <javaczysen@gmail.com>
2024-03-28 09:36:48 +0100

    first test for ReaderArticleRetrieverFacade

--- src/test/java/org/javne/reader_article_retriever/ReaderArticleRetrieverFacadeTest.java
+++ src/test/java/org/javne/reader_article_retriever/ReaderArticleRetrieverFacadeTest.java
@@ -1,15 +1,25 @@
 package org.javne.reader_article_retriever;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 class ReaderArticleRetrieverFacadeTest {
     @Test
-    public void f(){
-        //given
+    @DisplayName("should return article with id 1 and content '5 Kroków do sukcesu'")
+    public void shouldReturnArticleWithId1AndContent() {
+        // given
         ReaderArticleRetrieverFacade facade = new ReaderArticleRetrieverFacade();
-        //when
-        ArticleDto articleDto = facade.retrieveById(1L);
-        //then
 
+        // when
+        ArticleDto articleDto = facade.retrieveById(1L);
+
+        // then
+        assertEquals(1L, articleDto.id());
+        assertEquals("5 Kroków do sukcesu", articleDto.content());
+        assertEquals("Jak zostać programistą", articleDto.header());
     }
+
+
 }
